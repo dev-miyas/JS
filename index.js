90e16447d8df47e025c868296266caf2fad1d3b5
dev-miyas <devmiyas21@gmail.com>
2026-06-22 00:59:12 +0300

    practicing js variables

---
+++ index.js
@@ -0,0 +1,12 @@
+// //practice of const and let variables////
+//variables in js are dynamic that means we do not have to worry about the datatype it knows by itself
+const a = 10;
+console.log(a); // 10
+ a = 20; 
+ console.log(a); // throws an err we can not reassign a const variable
+ let name="Abebe"
+console.log(name); // Abebe
+name="Kebede"
+console.log(name); // Kebede b/c we can reassign a let variable
+
+/////////////////////////////////////////////////
